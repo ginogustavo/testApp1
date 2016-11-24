cb78efe22843b064419cb3cbbf2486944e6c60ef
Gustavo <ing.gino@gmail.com>
2016-11-24 01:23:58 -0500

    Applying format to the StartController

--- src/main/java/com/index/controller/StartController.java
+++ src/main/java/com/index/controller/StartController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.index.model.Cat;
 import com.index.repository.CatRepository;
@@ -19,24 +18,12 @@ public class StartController {
 				// Interface <- Class (the implementation), spring data magic.
 	private CatRepository catRepo;
 
-//	@RequestMapping(value = "/", method = RequestMethod.GET)
-//	// --> it associate to an URL, the root.
-//	public ModelAndView start() {
-//		List<Cat> petList = (List<Cat>) catRepo.findAll();
-//		ModelAndView mav = new ModelAndView("landing");
-//		mav.addObject("pets", petList);
-//		return mav;
-//	}
-
-	//TODO: change above method for the one in 02:06:55
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String landing(Model model){
+	// --> it associate to an URL, the root.
+	public String landing(Model model) {
 		List<Cat> petList = (List<Cat>) catRepo.findAll();
 		model.addAttribute("pets", petList);
 		return "landing";
 	}
-	
-	
-	
-	
+
 }
